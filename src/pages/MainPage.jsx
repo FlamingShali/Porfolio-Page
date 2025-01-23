09e9470cf9fa1ad1e65cb4a5e04ecce36237f5fd
Michał Klepner <michal.klepner1@o2.pl>
2025-01-23 20:38:29 +0100

    Added nav elements to main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,26 @@
+import { NavLink } from "react-router";
+
 const MainPage = () => {
   return (
-    <div className="text-white transition-all duration-300">
-      
+    <div className="text-white transition-all absolute duration-300 flex flex-col justify-center text-center text-xl m-auto left-0 right-0 top-40 font-semibold w-1/6">
+      <NavLink viewTransition
+        className="hover:scale-110 transition-all m-10 text-3xl sm:text-sm md:text-xl lg:text-2xl border-2 rounded-full shadow-[0px_0px_100px_10px_rgba(51,196,251,1)]  "
+        to="/aboutSection"
+      >
+        About
+      </NavLink>
+      <NavLink viewTransition
+        className="hover:scale-110 transition-all m-10 text-3xl sm:text-sm md:text-xl lg:text-2xl border-2 rounded-full shadow-[0px_0px_100px_10px_rgba(51,196,251,1)]  "
+        to="/projects"
+      >
+        My Projects
+      </NavLink>
+      <NavLink viewTransition
+        className="hover:scale-110 transition-all m-10 text-3xl sm:text-sm md:text-xl lg:text-2xl border-2 rounded-full shadow-[0px_0px_100px_10px_rgba(51,196,251,1)]  "
+        to="/contact"
+      >
+        Contact Me
+      </NavLink>
     </div>
   );
 };
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -3,7 +3,7 @@ import NavBar from "../components/NavBar";
 
 const AppLayout = () => {
   return (
-    <div className="relative w-screen h-screen bg-cover bg-center bg-[url(/src/images/theme.png)]">
+    <div className="relative w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url(/src/images/theme.png)]">
       <div className="absolute inset-0 bg-black opacity-60 blur-2xl"></div>{" "}
       {/* Przyciemnienie tła */}
       <div className="relative z-10">
