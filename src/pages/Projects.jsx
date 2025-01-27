fd1aa25c023ab4c5acc0d995b2a91b8b91e4e684
Michał Klepner <michal.klepner1@o2.pl>
2025-01-27 13:31:26 +0100

    Added animated project cards

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,11 @@
+import ProjectCards from "../components/ProjectCards";
+
 export default function Projects() {
   return (
     <div className="flex flex-col">
+      
       <div className="flex flex-row justify-between">
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-      </div>
-      <div className="flex flex-row justify-between">
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
-        <div className="w-64 h-80 border-2  m-auto mt-10 bg-transparent"></div>
+        <ProjectCards/>
       </div>
     </div>
   );
